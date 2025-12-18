d14274c87c833375db99577375f0eb1ed7432d2c
DavidMaksic <david.maksic88@gmail.com>
2025-12-18 18:37:04 +0100

    Fix for image revalidation

--- src/features/archive/useEditArticle.js
+++ src/features/archive/useEditArticle.js
@@ -1,22 +1,25 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useNavigate, useParams } from 'react-router-dom';
 import { updateArticle } from '../../services/apiArticles';
-import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
 export function useEditArticle() {
    const navigate = useNavigate();
    const queryClient = useQueryClient();
 
+   const { id } = useParams();
+   let articleID = id;
+   articleID = id.includes(':') ? id.split(':').at(1) : id;
+
    const { isPending: isEditing, mutate: editArticle } = useMutation({
       mutationFn: updateArticle,
-      onSuccess: () => [
-         toast.success('Article successfully edited'),
-         queryClient.invalidateQueries({
-            queryKey: ['articles'],
-         }),
-         navigate(-1),
-      ],
+      onSuccess: () => {
+         queryClient.invalidateQueries({ queryKey: ['article', articleID] });
+         toast.success('Article successfully edited');
+         navigate(-1);
+      },
       onError: (err) => toast.error(err.message),
    });
+
    return { isEditing, editArticle };
 }
