a2a4b5ab07a905bb3dc07a9d00d8715024520ede
DavidMaksic <david.maksic88@gmail.com>
2025-12-18 15:57:19 +0100

    Fix for images

--- api/revalidate.js
+++ api/revalidate.js
@@ -1,8 +1,11 @@
 export default async function handler(req, res) {
+   console.log('Request Body:', req.body);
+
    if (req.method !== 'POST')
       return res.status(405).json({ error: 'Method not allowed' });
 
-   const { slug } = req.body;
+   const { slug } = req.body || {};
+   if (!slug) return res.status(400).json({ error: 'Missing slug' });
 
    const response = await fetch(
       'https://ethos-blog.vercel.app/api/revalidate',
