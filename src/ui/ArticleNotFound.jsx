518e54b2112f45223cba4f4fa2099b5fbeab6447
DavidMaksic <david.maksic88@gmail.com>
2025-12-24 18:38:53 +0100

    Testing revalidation

--- src/ui/ArticleNotFound.jsx
+++ src/ui/ArticleNotFound.jsx
@@ -6,11 +6,11 @@ function ArticleNotFound({ to, prompt, children }) {
    return (
       <div className="max-h-screen flex justify-center">
          <div
-            className={`self-center bg-secondary dark:bg-primary-200 px-20 py-12 flex flex-col gap-10 rounded-3xl border border-quaternary translate-y-50 ${
+            className={`self-center box-shadow bg-secondary dark:bg-primary-200 px-20 py-12 flex flex-col gap-10 rounded-3xl border border-quaternary translate-y-50 ${
                !to && 'gap-0!'
             }`}
          >
-            <span className="text-3xl font-semibold">{children}</span>
+            <span className="text-3xl text-primary-500/70">{children}</span>
             <Link
                className="flex items-center gap-2 text-3xl w-max self-center text-primary-400 border-b-3 border-primary-400 hover:border-transparent dark:hover:border-transparent transition-all"
                to={to}
