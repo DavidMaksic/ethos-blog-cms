c4cfbc937685fcf6e2b6223991946f239d89550e
DavidMaksic <david.maksic88@gmail.com>
2025-12-31 15:41:06 +0100

    Visual fixes

--- src/ui/Operations/Filter.jsx
+++ src/ui/Operations/Filter.jsx
@@ -19,7 +19,7 @@ export function Filter({ field, options }) {
       <div className="flex items-center gap-4">
          <LuListFilter className="size-5 text-accent dark:text-accent-200" />
 
-         <div className="flex gap-2 bg-white dark:bg-primary-200 py-2 px-3 border border-tertiary dark:border-primary-300/10 rounded-xl shadow-2xs transition-bg_color_border">
+         <div className="flex gap-2 bg-white dark:bg-primary-300/15 py-2 px-3 border border-tertiary dark:border-primary-300/10 rounded-xl shadow-2xs transition-bg_color_border z-10">
             {options.map((item) => (
                <FilterButton
                   handler={() => handleClick(item.value)}
--- src/ui/Sidebar/Sidebar.jsx
+++ src/ui/Sidebar/Sidebar.jsx
@@ -2,8 +2,6 @@ import AuthorCard from './AuthorCard';
 import Logo from './Logo';
 import Nav from './Nav';
 
-// TODO: Add background gradients for each media query
-
 function Sidebar() {
    return (
       <aside className="sidebar fixed z-30 h-screen py-14 px-6 border-r row-span-full border-r-quaternary/90 dark:border-r-primary-300/20 [transition:transform_0.5s,background-color_0.3s,border_0.3s] ease-in-out flex flex-col justify-around">
