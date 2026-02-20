10b207e2803da857c8a097207eab54deb4053d19
DavidMaksic <david.maksic88@gmail.com>
2026-02-20 15:00:45 +0100

    Visual fixes

--- src/ui/Buttons/SubmitButton.jsx
+++ src/ui/Buttons/SubmitButton.jsx
@@ -15,10 +15,10 @@ function SubmitButton({
 
    const loadingStyle =
       isPending && btnClicked
-         ? 'from-white dark:from-transparent to-white dark:to-transparent !border-accent/80 dark:border-accent/80 shadow-none'
+         ? 'from-white dark:from-transparent to-white dark:to-transparent !border-accent/80 dark:border-accent/60 shadow-none'
          : '';
    const loadingTextStyle =
-      isPending && btnClicked ? '!text-accent dark:text-accent/90' : '';
+      isPending && btnClicked ? '!text-accent dark:text-accent/70!' : '';
 
    useEffect(() => {
       if (to && isSuccess) navigate(to);
@@ -27,10 +27,10 @@ function SubmitButton({
 
    return (
       <div
-         className={`rounded-full bg-gradient-to-r from-accent-300/80 to-accent-600/70 hover:from-white dark:hover:from-transparent hover:to-white dark:hover:to-transparent border-2 border-transparent hover:border-accent/80 dark:hover:border-accent/80 shadow-link-btn hover:shadow-none dark:shadow-none transition-[box-shadow,border,--tw-gradient-from,--tw-gradient-to] duration-300 bg-origin-border ${loadingStyle}`}
+         className={`rounded-full bg-gradient-to-r from-accent-300/80 dark:from-accent-300/60 to-accent-600/70 dark:to-accent-600/50 hover:from-transparent hover:to-transparent border-2 border-transparent hover:border-accent/70 dark:hover:border-accent/60 shadow-link-btn hover:shadow-none dark:shadow-none transition-[box-shadow,border,--tw-gradient-from,--tw-gradient-to] duration-300 bg-origin-border ${loadingStyle}`}
       >
          <button
-            className={`flex items-center gap-5 text-5xl px-10 pr-11 py-4 pt-5 text-white font-logo hover:text-accent dark:hover:text-accent/90 transition-[color] duration-300 disabled:!cursor-default ${loadingTextStyle}`}
+            className={`flex items-center gap-5 text-5xl px-10 pr-11 py-4 pt-5 text-white dark:text-accent-100/80 font-logo hover:text-accent/80 dark:hover:text-accent/70 transition-[color] duration-300 disabled:!cursor-default ${loadingTextStyle}`}
             type="submit"
             onClick={() => {
                if (handler) handler();
--- src/ui/Operations/FilterButton.jsx
+++ src/ui/Operations/FilterButton.jsx
@@ -2,7 +2,7 @@ function FilterButton({ handler, active, children }) {
    return (
       <button
          className={`relative z-10 py-0.5 px-2.5 rounded-xl transition-colors select-none ${
-            active && 'text-white dark:text-accent-50'
+            active && 'text-white dark:text-accent-100'
          } `}
          onClick={handler}
       >
