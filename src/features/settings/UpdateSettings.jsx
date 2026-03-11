2f26bb877301332dedc73c3f8809eb9ce326d72d
DavidMaksic <david.maksic88@gmail.com>
2026-03-11 14:04:14 +0100

    Visual fix

--- src/features/settings/UpdateSettings.jsx
+++ src/features/settings/UpdateSettings.jsx
@@ -1,6 +1,12 @@
+import { useLocation } from 'react-router-dom';
+
 function UpdateSettings({ children }) {
+   const { pathname } = useLocation();
+
    return (
-      <div className="bg-white dark:bg-primary-300/10 rounded-2xl px-10 transition-bg_border box-shadow transition-200 self-start">
+      <div
+         className={`bg-white dark:bg-primary-300/10 rounded-2xl px-10 transition-bg_border box-shadow transition-200 ${pathname === '/tags' ? 'self-start' : ''}`}
+      >
          {children}
       </div>
    );
